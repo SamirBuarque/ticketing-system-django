af96edd5f1278955f1857d95470fa215eca5d980
Samir Buarque <samirbuarque@hotmail.com>
2024-04-18 16:20:09 -0300

    modificando o arquivo views dos usuarios e criando o arquivo serializers

---
+++ userprofile/serializers.py
@@ -0,0 +1,4 @@
+"""
+classe responsável por converter os objetos do django, como modelos django, em dados primitivos como dicionários, que podem ser facilmente adicionados em arquivos JSON ou no banco de dados, e vice-versa.
+"""
+
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,55 +1,42 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from .models import UserProfile
-from django.contrib.auth import logout, login, authenticate
-from django.contrib.auth.decorators import login_required
-from django.conf import settings
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from . import models
+from . import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
-#@login_required
-def index(request):
-    if not request.user.is_authenticated:
-        return redirect("custom_login")
-        #return redirect(f"{settings.LOGIN_URL}?next={request.path}")
-    else:
-        return render(request, 'mysite/index.html')
+class EmployeeViewset(APIView):
+    def get(self, request, id=None):
+        if id:
+            item = models.Employee.objects.get(id=id)
+            serializer = serializers.EmployeeSerializer(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            UserProfile.objects.create(user=user)
+        items = models.Employee.objects.all()
+        serializer = serializers.EmployeeSerializer(items, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
 
-            return redirect('mysite/index.html')
-    else:    
-        form = UserCreationForm()
-    return render(request, "userprofile/signup.html", {'form': form})
-
-def custom_login(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-
-        if username and password:
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect("index")
-            else:
-                error_message = 'Erro ao efetuar login. Credenciais inválidas'
+    def post(self, request):
+        serializer = serializers.EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
-            error_message = 'Por favor preencha todos os campos'
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-        return render(request, "userprofile/login.html", {'error_message': error_message})
-              
-    else:
-        return render(request, "userprofile/login.html")
+    def patch(self, request, id=None):
+        item = models.Employee.objects.get(id=id)
+        serializer = serializers.EmployeeSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-def custom_logout(request):
-    logout(request)
-    return redirect("custom_login")
-    
-    
-
+    def delete(self, request, id=None):
+        item = models.Employee.objects.filter(id=id)
+        print(item)
+        item.delete()
+        return Response({"status": "success", "data": "Item Deleted"})
