499553e44fe00e5de9f0ed0a83d1d548547be1d7
Samir Buarque <samirbuarque@hotmail.com>
2024-04-19 14:54:15 -0300

    :construction: feat(settings/models): Alterando o timezone | criando um modelo para os usuários

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,5 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
+from django.utils import timezone
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)
+
+    # adiciona automaticamente a data de criação desse usuario
+    #created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    """ Armazenar no banco de dados o hash gerado """
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    
