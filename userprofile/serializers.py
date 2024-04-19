0de4cfa5eadd8b4a707617b3353cb8d8697972a3
Samir Buarque <samirbuarque@hotmail.com>
2024-04-19 15:05:00 -0300

    :construction: feat(serializers): Criando a classe serializer dos usuarios

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -2,3 +2,10 @@
 classe responsável por converter os objetos do django, como modelos django, em dados primitivos como dicionários, que podem ser facilmente adicionados em arquivos JSON ou no banco de dados, e vice-versa.
 """
 
+from rest_framework.serializers import ModelSerializer
+from .models import UserProfile
+
+class UserProfileSerializer(ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
