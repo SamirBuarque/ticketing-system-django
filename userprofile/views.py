54846264e34c8e040710cf72646fc130f2b49f8a
Samir Buarque <samirbuarque@hotmail.com>
2024-04-19 21:35:13 -0300

    :construction: feat(views -> userprofile): criando as funções de CRUD da API

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,34 +1,35 @@
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
-from . import models
-from . import serializers
+from models import UserProfile
+from serializers import UserProfileSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 
-class EmployeeViewset(APIView):
+class UserProfileViewset(APIView):
     def get(self, request, id=None):
         if id:
-            item = models.Employee.objects.get(id=id)
-            serializer = serializers.EmployeeSerializer(item)
+            # item = objeto com id único gerado automaticamente pelo framework
+            item = UserProfile.objects.get(id=id)
+            serializer = UserProfileSerializer(item)
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
 
-        items = models.Employee.objects.all()
-        serializer = serializers.EmployeeSerializer(items, many=True)
+        items = UserProfile.objects.all()
+        serializer = UserProfileSerializer(items, many=True)
         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
 
     def post(self, request):
-        serializer = serializers.EmployeeSerializer(data=request.data)
+        serializer = UserProfileSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save() # inserindo na tabela
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, id=None):
-        item = models.Employee.objects.get(id=id)
-        serializer = serializers.EmployeeSerializer(item, data=request.data, partial=True)
+        item = UserProfile.objects.get(id=id)
+        serializer = UserProfileSerializer(item, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
@@ -36,7 +37,7 @@ class EmployeeViewset(APIView):
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id=None):
-        item = models.Employee.objects.filter(id=id)
+        item = UserProfile.objects.filter(id=id)
         print(item)
         item.delete()
         return Response({"status": "success", "data": "Item Deleted"})
